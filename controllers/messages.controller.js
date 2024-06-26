f376e37996482fc456074e8cc876ea9815c2e258
Vivan-verma1109 <vivanv732@gmail.com>
2024-06-26 12:09:18 -0700

    image_testing

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  res.send("<ul><li>pbc </li> </ul>");
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'))
+  
+  //res.send("<ul><li>pbc </li> </ul>");
 }
 
 function postMessages(req, res) {
