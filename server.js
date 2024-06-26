3339db335c4d7cb19512d87b6851f254c8aab04e
Vivan-verma1109 <vivanv732@gmail.com>
2024-06-26 12:30:47 -0700

    implementing hbs

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -19,6 +22,12 @@ app.use("/site", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Cheesecake slaps",
+    caption: "Pretty Views",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
